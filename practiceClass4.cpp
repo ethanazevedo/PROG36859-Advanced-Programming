e08c7e4f51a7c2066dcaf84b163e05bb0fdf73e0
noobb <251897851+ethanazevedo@users.noreply.github.com>
2026-09-11 20:46:14 -0400

    testing

--- practiceClass4.cpp
+++ practiceClass4.cpp
@@ -8,7 +8,7 @@ class Product{
         double price;
         int amount;
     public:
-        Product();
+        Product(): id(0), name(""), price(0.0), amount(0) {};
         Product(int id, string name, double price, int amount): id(id), name(name), price(price), amount(amount){};
 
         int getId(){
@@ -28,17 +28,46 @@ class Product{
         };
 
         void setId(int i){
-            id = i
+            id = i;
         };
 
         void setName(string n){
             name = n;
         };
 
+        void setPrice(double p){
+            price = p;
+        };
+
+        void setAmount(int a){
+            amount = a;
+        };
+
+        void print(){
+            cout << "ID: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "Price: " << price << endl;
+            cout << "Amount: " << amount << endl;
+        };
 
 };
 
 int main(){
+
+    Product p1;
+    Product p2(124, "Banana", 2.79, 1);
+
+    cout << p2.getId() << endl;
+    cout << p2.getName() << endl;
+    cout << p2.getPrice() << endl;
+    cout << p2.getAmount() << endl << endl;
+
+    p1.setId(456);
+    p1.setName("Strawberry");
+    p1.setPrice(1.99);
+    p1.setAmount(8);
+
+    p1.print();
 
     return 0;
 }
